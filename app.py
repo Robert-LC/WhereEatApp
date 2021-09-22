d2a5d4d60c1c1e88e51a6b04278245fbe98cafd0
Robert LoCicero <Robert-LC>
2021-09-22 01:47:28 -0400

    Integrated ZipCode API to get data from user zip code

--- app.py
+++ app.py
@@ -1,10 +1,40 @@
-from flask import Flask, render_template # Used to take the index.html to render it when flask is ran.
-import requests
-import json
+from flask import Flask, render_template, request
+import config # File wih API keys
+import requests # Webcalls
+import pandas as pd #Turn response into a dataframe
+
+
 
 #Instantiates the Flask App
 app = Flask(__name__)
 
-@app.route("/") # Decorator that flask uses to assign the url
-def get_index():
-    return render_template("index.html")
+@app.route('/', methods=['POST']) # Decorator that flask uses to assign the url
+def getIndex():
+    return render_template('index.html')
+
+@app.route('/results')
+def getResults():
+    data = searchZipCode(37604) 
+    return render_template('results.html', data=data)
+
+# Function gets response from ZipCode Api, converts it to JSON, then turns JSON into a dictionary of info with user entered zipcode.
+def searchZipCode(zip):
+    r = requests.get(f'https://www.zipcodeapi.com/rest/{config.ZipKey}/info.json/{zip}/degrees')
+    zipJson = r.json()
+
+    # Pass the zip Codes info (City, State, Long, Lat into a dictionary)
+    zipCodeDict = {
+        'city' : zipJson['city'],
+        'state' : zipJson['state'],
+        'lattitude' : zipJson['lat'],
+        'longitude' : zipJson['lng'],
+    }
+    return zipCodeDict
+    
+    
+def getUserSubmit():
+    zipCode = request.form.get('zipCode')
+    return zipCode
+
+if __name__ == '__main__':
+        app.run(debug=True)
---
+++ config.py
@@ -0,0 +1,9 @@
+# Credentials: key, url, headers
+#url = 'https://api.yelp.com/v3/businesses/search'
+#key = '2Xsnxr6W3oB0XCDQDtsKOkRnoUf-AB0YmW9JYd1w7J_9DC6e0V22KTjhm_eWUWP6WXVqimqzqMdakTyUPVBVp4_ChtbdksM87PJHciPZ9VXeVdJFi54PwOQ8vIwSYXYx'
+#headers = {
+    #'Authorization': 'Bearer %s' % key
+#}
+
+ 
+ZipKey = "VcUnPfEHK63jVxarN8U5PmsHyJ3eiPnsS00hTZ23X0boq7HgLW895nizGTx2fsd1"
